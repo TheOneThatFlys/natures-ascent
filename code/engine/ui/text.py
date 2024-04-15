b37c9af3b1d583e329f760ac4a4f10418c115c3f
TheOneThatFlys <angranyue@gmail.com>
2024-04-15 20:13:03 +0100

    changed text shadow to be vertical only

--- code/engine/ui/text.py
+++ code/engine/ui/text.py
@@ -19,7 +19,7 @@ class Text(Element):
             base_img = pygame.Surface((self.image.get_width() + self.style.text_shadow, self.image.get_height() + self.style.text_shadow), pygame.SRCALPHA)
             shadow_image = self.style.font.render(self.text, False, self.style.colour)
 
-            base_img.blit(shadow_image, (self.style.text_shadow, self.style.text_shadow))
+            base_img.blit(shadow_image, (0, self.style.text_shadow))
             base_img.blit(self.image, (0, 0))
             self.image = base_img
 
