19e7ff080129ccb33d38e65f0d3d8f14bfff5c4b
TheOneThatFlys <angranyue@gmail.com>
2024-04-19 19:54:15 +0100

    prettified debug window with rich text

--- code/util/debug_window.py
+++ code/util/debug_window.py
@@ -6,8 +6,10 @@ from engine import Node, Logger, Manager, Screen
 from engine.ui import *
 from engine.types import *
 
+from .misc import render_rich_text
+
 class DebugWindow(Screen):
-    ALLOWED_REC = (Node, Manager, Style, list, tuple)
+    ALLOWED_REC = (Node, Manager, Style, list, dict, set)
     def __init__(self, parent: Node) -> None:
         super().__init__(parent)
         self.window = pygame.Window("Nature's Ascent - Debug", (640, 480))
@@ -23,77 +25,115 @@ class DebugWindow(Screen):
         # focus back to main game
         game_window.focus()
 
-        self.font = pygame.font.SysFont("Consolas", 12)
+        #self.font = self.manager.get_font("cascadia_cove", 12)
+        self.font = pygame.font.SysFont("Consolas", 12, bold = True)
         # pygame.font.Font.size() doesn't seem to calculate the right height so use this hardcoded value instead
-        self.line_height = 15
+        self.line_height = self.font.get_linesize()
 
-        self.tab_length = self.font.size("      ")[0]
+        self.tab_length = self.font.size("  ")[0]
+        self.tab_line_offset = self.font.size(" |")[0] / 2
         self.padding_left = 4
         self.padding_top = 4
 
-        self.text_colour = (255, 255, 255)
         self.background_colour = (40, 42, 54)
-        self.background_colour_2 = (33, 34, 44)
 
-        self.expanded_folders: set[Node] = set()
-        self.folder_buttons: dict[Node, pygame.Rect] = []
+        self.text_colour = (255, 255, 255)
+        self.text_colour_2 = (62, 64, 74)
+        self.text_colour_faint = (98, 114, 164)
+
+        self.type_colour = (139, 233, 253)
+        self.op_colour = (255, 121, 198)
+        self.num_colour = (189, 147, 249)
+        self.str_colour = (241, 250, 129)
+
+        self.expanded_folders: set[str] = set()
+        self.folder_buttons: dict[str, pygame.Rect] = []
+        self.expanded_folders.add("/Game")
 
         self.scroll_offset = 0
 
-    def get_game_data(self) -> dict:
-        """Traverse through the whole game and extract every node and its values"""
-        raise NotImplementedError()
-
-    def render_line(self, text: str, tab_index) -> pygame.Rect:
+    def render_line(self, rich_text: str, tab_index: int) -> pygame.Rect:
         position = pygame.Vector2(
-            tab_index * self.tab_length + self.padding_left,
+            self.padding_left,
             self.current_line * self.line_height + self.scroll_offset + self.padding_top
         )
         self.current_line += 1
         if position.y < -50 or position.y > self.window.size[1]:
             return None
-        
-        # draw text
-        text_surf = self.font.render(text, True, self.text_colour)
-        self.render_surface.blit(text_surf, position)
-        return text_surf.get_rect(topleft = position)
 
-    def render_folder(self, folder_name: str, folder_value: Node, tab_index: int) -> pygame.Rect:
-        icon = "▾" if folder_value in self.expanded_folders else "▸"
-        return self.render_line(f"{icon} {folder_name}", tab_index)
+        for i in range(tab_index):
+            pygame.draw.line(self.render_surface, self.text_colour_2, (i * self.tab_length + self.tab_line_offset, position.y), (i * self.tab_length + self.tab_line_offset, position.y + self.line_height))
+
+        # draw text
+        s = render_rich_text(self.font, rich_text)
+        self.render_surface.blit(s, position + pygame.Vector2(tab_index * self.tab_length, 0))
+        return pygame.Rect(0, position.y, self.window.size[0], s.get_height())
+
+    def render_folder(self, rich_text: str, folder_path: str, tab_index: int) -> pygame.Rect:
+        icon = "▾" if folder_path in self.expanded_folders else "▸"
+        return self.render_line(f"%{self.text_colour}{icon} {rich_text}", tab_index)
 
     def __group_dict_items(self, dict_items: list[tuple]) -> list[tuple]:
-        return map(lambda x: (x[0], tuple(x[1])) if isinstance(x[1], list) else x, sorted(dict_items, key = lambda str_val: isinstance(str_val[1], self.ALLOWED_REC)))
+        return sorted(dict_items, key = lambda str_val: (not isinstance(str_val[1], self.ALLOWED_REC), str_val[0]))
 
     def render_lists(self) -> dict:
-        seen = list()
-        def __rec_render(d: dict, tab_index) -> None:
+        def __rec_render(d: dict, path: str, depth: int) -> None:
             for k, v in self.__group_dict_items(d.items()):
-                if k == "parent" or v in seen: continue
+                if k.startswith("_Sprite_"): continue
+                # calculate folder path of this item
+                path_to_item = f"{path}/{k}"
+
+                type_str = f"%{self.type_colour}{v.__class__.__name__}"
+                name_str = f"%{self.text_colour}{'' if k.startswith('$$') else k}"
+
+                # if not a base value
                 if isinstance(v, self.ALLOWED_REC):
-                    seen.append(v)
-                    bounding_rect = self.render_folder(k, v, tab_index)
-                    if bounding_rect: self.folder_buttons[v] = bounding_rect
-                    if v in self.expanded_folders:
-                        if isinstance(v, (list, tuple)):
-                            __rec_render({i: v for i, v in enumerate(v)}, tab_index + 1)
+                    # render the folder line and get bounding rect
+                    folder_text = f"{type_str} {name_str}"
+                    if isinstance(v, (list, tuple, set, dict)):
+                        folder_text += f" %{self.text_colour_faint}({len(v)})"
+                    bounding_rect = self.render_folder(folder_text, path_to_item, depth)
+                    if bounding_rect:
+                        # add a folder button
+                        self.folder_buttons[path_to_item] = bounding_rect
+
+                    # if the folder is expanded, render children
+                    if path_to_item in self.expanded_folders:
+                        thing_to_render: dict[str, Any]
+                        if isinstance(v, (list, set)):
+                            thing_to_render = {f"$${str(index)}": value for index, value in enumerate(v)}
+                        elif isinstance(v, dict):
+                            thing_to_render = v
                         else:
-                            __rec_render(v.__dict__, tab_index + 1)
+                            thing_to_render = v.__dict__
+                        __rec_render(thing_to_render, path_to_item, depth + 1)
                 else:
-                    self.render_line(f"{k} = {v}", tab_index)
+                    v_type = v.__class__
+                    v_colour = self.text_colour
+                    if v_type == int or v_type == float or v_type == bool:
+                        v_colour = self.num_colour
+                    elif v_type == str:
+                        v_colour = self.str_colour
+
+                    value = v
+                    if isinstance(v, str):
+                        value = v.__repr__()
+                    value_str = f"%{v_colour}{value}"
+                    op_str = f"%{self.op_colour}{'' if k.startswith('$$') else ' = '}"
+                    self.render_line(f"{type_str} {name_str}{op_str}{value_str}", depth)
 
         self.current_line = 0
         self.folder_buttons = {}
-        __rec_render(self.manager.game.__dict__, 0)
+        __rec_render({"Game": self.manager.game.__dict__}, "", 0)
 
     def on_mouse_down(self, button: int) -> None:
         mouse_pos = self.manager.get_mouse_pos("debug")
-        for node, rect in self.folder_buttons.items():
+        for path, rect in self.folder_buttons.items():
             if rect.collidepoint(mouse_pos):
-                if node in self.expanded_folders:
-                    self.expanded_folders.remove(node)
+                if path in self.expanded_folders:
+                    self.expanded_folders.remove(path)
                 else:
-                    self.expanded_folders.add(node)
+                    self.expanded_folders.add(path)
 
     def on_scroll(self, dx: int, dy: int) -> None:
         self.scroll_offset += dy * 20
