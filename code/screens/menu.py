cf36572cd285ce55fbaa3b7c0c3514f9ba33ee83
TheOneThatFlys <angranyue@gmail.com>
2024-06-10 21:51:25 +0100

    added small disclaimer

--- code/screens/menu.py
+++ code/screens/menu.py
@@ -1,4 +1,3 @@
-
 import pygame
 
 from engine import Screen
@@ -101,7 +100,6 @@ class Menu(Screen):
             on_click = self.parent.set_screen,
             click_args = ["credits"],
             hover_sound = None,
-            click_sound = "effect/hit_alt"
         ))
 
         self.manager.play_sound(sound_name = "music/menu", loop = True)
@@ -121,6 +119,32 @@ class Menu(Screen):
     def update(self) -> None:
         self.master_container.update()
 
+class ScrollableText(Element):
+    def __init__(self, parent: Element, style: Style, text: str, text_padding: int = 0, scroll_strength: int = 30) -> None:
+        self.text = text
+        self.text_padding = text_padding
+        self.scroll_strength = scroll_strength
+
+        self._scroll_progress = 0
+
+        super().__init__(parent, style)
+
+    def redraw_image(self) -> None:
+        super().redraw_image()
+        font_surf = self.style.font.render(self.text, self.style.antialiasing, self.style.fore_colour, wraplength = self.style.size[0] - 2 * self.text_padding)
+        self.image.blit(font_surf, (self.text_padding, self.text_padding + self._scroll_progress))
+
+    def on_scroll(self, dx: int, dy: int) -> None:
+        super().on_scroll(dx, dy)
+
+        if self.rect.collidepoint(self.manager.get_mouse_pos()):
+            self._scroll_progress += dy * self.scroll_strength
+
+            if self._scroll_progress > 0:
+                self._scroll_progress = 0
+
+            self.redraw_image()
+
 class CreditsScreen(Screen):
     def __init__(self, parent) -> None:
         super().__init__(parent)
@@ -137,7 +161,7 @@ class CreditsScreen(Screen):
         self.title = self.master_container.add_child(
             Text(
                 parent = self.master_container,
-                text = "Credits",
+                text = "Legal Disclaimer",
                 style = Style(
                     alignment = "top-center",
                     offset = (0, 64),
@@ -150,45 +174,43 @@ class CreditsScreen(Screen):
         )
 
         self.text = self.master_container.add_child(
-            Text(
+            ScrollableText(
                 parent = self.master_container,
-                text = "Lorem ipsum dolor sit amet\n, consectetur adipiscing elit.",
+                text = 
+"""
+By playing this game, you are submitting to the jurisdiction and internal governance of the developers. Any and all legal action will be met with equal and opposite opposition, and may, or may not, result in the incapacitation or permanant disfiguration of all parties involved.
+
+The developers of this game are not liable for any damages that occur during the consumption of the game. This is including, but not limited to: heatstroke, headaches, back pain, rabies, acne, stroke, chest pain, miscarriage, shingles, osteoporosis, flooding, volcano eruption, avalanche, tsunami, ovarian cysts, unknown discharges of any colour, sudden death, wildfires, dizziness, loss of smell, constipation, false sense of wellbeing, eternal happiness.
+
+This game created for entertainment purposes only. Any attempt to to use this product in an educational, promotional, or any other manner may result in unintended behaviour. This product is not to be used as protection from natural disasters, and repeated use may result in severe damage to the user's reproductive system. Reheating this product may result in the growth of unclassified bacteria and fungi; the developers take no responsibility for any and all resulting plagues or pandemics arisen from this.
+""",
                 style = Style(
                     alignment = "top-center",
                     offset = (0, self.title.rect.bottom + 16),
-                    font = self.manager.get_font("alagard", 32),
+                    font = self.manager.get_font("alagard", 16),
+                    size = (self.rect.width - 100, self.rect.height - 200),
+                    colour = (0, 0, 0, 0),
                     fore_colour = (255, 255, 255),
                 )
             )
         )
-    def render_multiline_text(self, text: str, font: pygame.font.Font, colour: Colour) -> pygame.Surface:
-        line_images = []
-        for line in text.split("\n"):
-            s = font.render(line, False, colour)
-            line_images.append(s)
-
-        width = max(line_images, key = lambda s: s.get_width())
-        height = sum(line_images, key = lambda s: s.get_height())
-        image = pygame.Surface((width, height), pygame.SRCALPHA)
-
-        ty = 0
-        for l in line_images:
-            r = l.get_rect(centerx = width / 2, y = ty)
-            image.blit(l, r)
-            ty += r.height
-
-        return image
 
     def on_key_down(self, key: int, unicode: str) -> None:
         super().on_key_down(key, unicode)
         if key == pygame.K_ESCAPE:
             self.parent.set_screen("menu")
 
+    def on_scroll(self, dx: int, dy: int) -> None:
+        super().on_scroll(dx, dy)
+        self.master_container.on_scroll(dx, dy)
+
     def on_resize(self, new_res: Vec2) -> None:
         super().on_resize(new_res)
         self.master_container.style.size = new_res
         self.master_container.style.image = util.draw_background(new_res)
+        self.text.style.size = (self.rect.width - 100, self.rect.height - 200)
         self.master_container.on_resize(new_res)
+
 
     def render(self, window: pygame.Surface) -> None:
         self.master_container.render(window)
